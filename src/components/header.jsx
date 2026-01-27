5365257f481c0e6c0746544afe8aa57c6125f06c
ChrisOspina <ChrisOspina99@gmail.com>
2026-01-27 15:09:39 -0500

    authentication fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,40 +9,70 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { LinkIcon, LogOut } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { UrlState } from "@/context";
+import useFetch from "@/hooks/use-fetch";
+import { BarLoader } from "react-spinners";
+import { logout } from "@/db/apiAuth";
 
 const Header = () => {
   const navigate = useNavigate();
-  const user = false;
-  return (
-    <nav className="py-4 flex justify-between items-center">
-      <Link to="/">
-        <img src="/src/assets/anipsobw.png" className="h-12" alt="Logo" />
-      </Link>
 
-      <div>
-        {!user ? (
-          <Button onClick={() => navigate("/auth")}>Login</Button>
-        ) : (
-          <DropdownMenu>
-            <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </div>
-    </nav>
+  const { user, fetchUser } = UrlState();
+
+  const { loading, fn: fnLogout } = useFetch(logout);
+
+  return (
+    <>
+      <nav className="py-4 flex justify-between items-center">
+        <Link to="/">
+          <img src="/src/assets/anipsobw.png" className="h-12" alt="Logo" />
+        </Link>
+
+        <div>
+          {!user ? (
+            <Button onClick={() => navigate("/auth")}>Login</Button>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+                <Avatar>
+                  <AvatarImage src={user?.user_metadata?.profile_pic} />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  {" "}
+                  {user?.user_metadata?.name}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link to="/dashboard" className="flex">
+                    <LinkIcon className="mr-2 h-4 w-4" />
+                    My Links
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span
+                    onClick={() => {
+                      fnLogout().then(() => {
+                        fetchUser();
+                        navigate("/");
+                      });
+                    }}
+                  >
+                    Logout
+                  </span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+      </nav>
+      {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
+    </>
   );
 };
 
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { Input } from "./ui/input";
 import {
   Card,
   CardContent,
@@ -6,27 +6,28 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "./ui/input";
+} from "./ui/card";
 import { Button } from "./ui/button";
-import { BeatLoader } from "react-spinners";
-import Error from "./error";
-import * as Yup from "yup";
-import useFetch from "@/hooks/use-fetch";
-import { login } from "@/db/apiAuth";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import * as Yup from "yup";
+import Error from "./error";
+import { login } from "@/db/apiAuth";
+import { BeatLoader } from "react-spinners";
+import useFetch from "@/hooks/use-fetch";
 import { UrlState } from "@/context";
 
 const Login = () => {
+  let [searchParams] = useSearchParams();
+  const longLink = searchParams.get("createNew");
+
+  const navigate = useNavigate();
+
   const [errors, setErrors] = useState({});
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
-
-  const navigate = useNavigate;
-  let [searchParams] = useSearchParams();
-  const longLink = searchParams.get("createNew");
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -41,19 +42,18 @@ const Login = () => {
 
   useEffect(() => {
     if (error === null && data) {
-      navigate(`/dashboard${longLink ? "?createNew=true" : ""}`);
       fetchUser();
-    } else if (error) {
-      setErrors({ message: error.message });
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
     }
-  }, [data, error, fetchUser, longLink, navigate]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error, data]);
 
   const handleLogin = async () => {
-    setErrors({});
+    setErrors([]);
     try {
       const schema = Yup.object().shape({
         email: Yup.string()
-          .email("Invalid email format")
+          .email("Invalid email")
           .required("Email is required"),
         password: Yup.string()
           .min(6, "Password must be at least 6 characters")
@@ -61,53 +61,53 @@ const Login = () => {
       });
 
       await schema.validate(formData, { abortEarly: false });
-      //api call would go here
       await fnLogin();
-    } catch (err) {
+    } catch (e) {
       const newErrors = {};
 
-      err?.inner?.forEach((error) => {
-        newErrors[error.path] = error.message;
+      e?.inner?.forEach((err) => {
+        newErrors[err.path] = err.message;
       });
+
       setErrors(newErrors);
     }
   };
 
   return (
-    <div>
-      <Card>
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>Sign in to your account</CardDescription>
-          {errors && <Error message={errors.message} />}
-        </CardHeader>
-        <CardContent className="space-y-2">
-          <div className="space-y-1">
-            <Input
-              name="email"
-              type="email"
-              placeholder="Enter Email"
-              onChange={handleInputChange}
-            />
-            {errors.email && <Error message={errors.email} />}
-          </div>
-          <div className="space-y-1">
-            <Input
-              name="password"
-              type="password"
-              placeholder="Enter Password"
-              onChange={handleInputChange}
-            />
-            {errors.password && <Error message={errors.password} />}
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button onClick={handleLogin} className="w-full">
-            {loading ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
-          </Button>
-        </CardFooter>
-      </Card>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          to your account if you already have one
+        </CardDescription>
+        {error && <Error message={error.message} />}
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
+          <Input
+            name="email"
+            type="email"
+            placeholder="Enter Email"
+            onChange={handleInputChange}
+          />
+        </div>
+        {errors.email && <Error message={errors.email} />}
+        <div className="space-y-1">
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter Password"
+            onChange={handleInputChange}
+          />
+        </div>
+        {errors.password && <Error message={errors.password} />}
+      </CardContent>
+      <CardFooter>
+        <Button onClick={handleLogin}>
+          {loading ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 
