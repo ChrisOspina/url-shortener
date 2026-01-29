a6741b4dfdcd092eefe2182effca1c6c6672e925
ChrisOspina <ChrisOspina99@gmail.com>
2026-01-29 15:58:21 -0500

    dialog created for url creation

--- src/components/create-link.jsx
+++ src/components/create-link.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -13,34 +14,126 @@ import { Card } from "./ui/card";
 import { Button } from "./ui/button";
 import Error from "./error";
 import { Input } from "./ui/input";
+import * as Yup from "yup";
+import QRCode from "react-qrcode-logo";
+import useFetch from "@/hooks/use-fetch";
+import { createUrl } from "@/db/apiUrls";
+import { BeatLoader } from "react-spinners";
 
 const CreateLink = () => {
   const { user } = UrlState();
+
   const navigate = useNavigate();
+  const ref = useRef();
 
   let [searchParams, setSearchParams] = useSearchParams();
   const longLink = searchParams.get("createNew");
 
+  const [errors, setErrors] = React.useState({});
+  const [formValues, setFormValues] = useState({
+    title: "",
+    longUrl: longLink ? longLink : "",
+    customUrl: "",
+  });
+
+  const schema = Yup.object().shape({
+    title: Yup.string().required("Title is required"),
+    longUrl: Yup.string()
+      .url("Invalid URL format")
+      .required("Long URL is required"),
+    customUrl: Yup.string().matches(
+      /^[a-zA-Z0-9-_]*$/,
+      "Custom URL can only contain letters, numbers, hyphens, and underscores",
+    ),
+  });
+
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const {
+    loading,
+    error,
+    data,
+    fn: fnCreateUrl,
+  } = useFetch(createUrl, { ...formValues, user_id: user?.id });
+
+  useEffect(() => {
+    if (error === null && data) {
+      navigate(`/link/${data[0].id}`);
+    }
+  }, [error, data]);
+
+  const createNewLink = async () => {
+    setErrors([]);
+    try {
+      await schema.validate(formValues, { abortEarly: false });
+      const canvas = ref.current.canvasRef.current;
+      const blob = await new Promise((resolve) => canvas.toBlob(resolve));
+
+      await fnCreateUrl(blob);
+    } catch (e) {
+      const newErrors = {};
+
+      e?.inner?.forEach((err) => {
+        newErrors[err.path] = err.message;
+      });
+      setErrors(newErrors);
+    }
+  };
+
   return (
-    <Dialog>
-      <DialogTrigger>
+    <Dialog
+      defaultOpen={longLink}
+      onOpenChange={(res) => {
+        if (!res) setSearchParams({});
+      }}
+    >
+      <DialogTrigger asChild>
         <Button variant="ghost">Create New Link</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-md">
         <DialogHeader>
           <DialogTitle className="font-bold text-2xl">Create New</DialogTitle>
         </DialogHeader>
-        <Input id="title" placeholder="Short Link's Title" />
-        <Error message={"An error occurred"} />
-        <Input id="longUrl" placeholder="Enter your Loooong URL" />
-        <Error message={"An error occurred"} />
+        {formValues?.longUrl && (
+          <QRCode value={formValues.longUrl} size={128} ref={ref} />
+        )}
+        <Input
+          id="title"
+          placeholder="Short Link's Title"
+          value={formValues.title}
+          onChange={handleChange}
+        />
+        {errors.title && <Error message={errors.title} />}{" "}
+        <Input
+          id="longUrl"
+          placeholder="Enter your Loooong URL"
+          value={formValues.longUrl}
+          onChange={handleChange}
+        />
+        {errors.longUrl && <Error message={errors.longUrl} />}{" "}
         <div className="flex items-center gap-2">
           <Card className="p-2">trimrr.in</Card> /
-          <Input id="customUrl" placeholder="Custom Link (optional)" />
+          <Input
+            id="customUrl"
+            placeholder="Custom Link (optional)"
+            value={formValues.customUrl}
+            onChange={handleChange}
+          />
         </div>
-        <Error message={"An error occurred"} />
+        {error && <Error message={errors.message} />}
         <DialogFooter>
-          <Button variant="destructive">Create Link</Button>
+          <Button
+            disabled={loading}
+            onClick={createNewLink}
+            variant="destructive"
+          >
+            {loading ? <BeatLoader size={10} color="white" /> : "Create"}{" "}
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
